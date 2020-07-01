ac866ae8e81880b68b7902f2a467dd3783fbed35
Pagan Gazzard <page@balena.io>
2020-07-01 15:35:47 +0100

    Update to @balena/lint 5.x
    Change-type: major

--- lib/node-storage.ts
+++ lib/node-storage.ts
@@ -40,7 +40,7 @@ export class NodeStorage implements StorageLike {
 	public async clear() {
 		try {
 			await Promise.all(
-				(await fs.readdir(this.dataDirectory)).map(async f => {
+				(await fs.readdir(this.dataDirectory)).map(async (f) => {
 					f = path.join(this.dataDirectory, f);
 					try {
 						if ((await fs.stat(f)).isDirectory()) {
