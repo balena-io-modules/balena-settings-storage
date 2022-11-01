33262bd764f9e88cd566bf49939bb017ac7dd47b
Thodoris Greasidis <thgreasi@gmail.com>
2022-11-01 20:01:36 +0200

    Fix tests on node18
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- tests/storage.spec.ts
+++ tests/storage.spec.ts
@@ -1,8 +1,12 @@
+import * as chai from 'chai';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as chaiAsPromised from 'chai-as-promised';
 import * as BalenaSettingsClientModule from 'balena-settings-client';
 import * as FsModule from 'fs';
-import * as m from 'mochainon';
 import * as path from 'path';
 import { BalenaSettingsPermissionError } from 'balena-errors';
+chai.use(chaiAsPromised);
 
 import { createStorage } from '../lib/local-storage';
 import getStorage = require('../lib/storage');
@@ -31,45 +35,42 @@ describe('Storage:', () => {
 	describe('given numbers', () => {
 		it('should be able to save a float number', () =>
 			storage.set('pi', 3.14).then(() => {
-				return m.chai.expect(storage.get('pi')).to.eventually.equal(3.14);
+				return expect(storage.get('pi')).to.eventually.equal(3.14);
 			}));
 
 		it('should be able to save an integer number', () =>
 			storage.set('age', 34).then(() => {
-				return m.chai.expect(storage.get('age')).to.eventually.equal(34);
+				return expect(storage.get('age')).to.eventually.equal(34);
 			}));
 
 		it('should be able to save a string number containin numbers as a string', () =>
 			storage.set('pi', 'pi=3.14').then(() => {
-				return m.chai.expect(storage.get('pi')).to.eventually.equal('pi=3.14');
+				return expect(storage.get('pi')).to.eventually.equal('pi=3.14');
 			}));
 	});
 
 	describe('given objects', () => {
 		it('should be able to save a plain object', () =>
 			storage.set('pi', { value: 3.14 }).then(() => {
-				return m.chai
-					.expect(storage.get('pi'))
-					.to.eventually.deep.equal({ value: 3.14 });
+				return expect(storage.get('pi')).to.eventually.deep.equal({
+					value: 3.14,
+				});
 			}));
 
 		it('should be able to save an empty object', () =>
 			storage.set('empty', {}).then(() => {
-				return m.chai.expect(storage.get('empty')).to.eventually.deep.equal({});
+				return expect(storage.get('empty')).to.eventually.deep.equal({});
 			}));
 	});
 
 	describe('.set()', () => {
 		describe('given a key does not exist', () => {
 			it('should be able to set a value', () => {
-				return m.chai
-					.expect(storage.get('foobar'))
+				return expect(storage.get('foobar'))
 					.to.eventually.equal(undefined)
 					.then(() => storage.set('foobar', 'Hello'))
 					.then(() => {
-						return m.chai
-							.expect(storage.get('foobar'))
-							.to.eventually.equal('Hello');
+						return expect(storage.get('foobar')).to.eventually.equal('Hello');
 					});
 			});
 		});
@@ -80,12 +81,11 @@ describe('Storage:', () => {
 			afterEach(() => storage.remove('foobar'));
 
 			it('should be able to change the value', () => {
-				return m.chai
-					.expect(storage.get('foobar'))
+				return expect(storage.get('foobar'))
 					.to.eventually.equal('Hello')
 					.then(() => storage.set('foobar', 'World'))
 					.then(() =>
-						m.chai.expect(storage.get('foobar')).to.eventually.equal('World'),
+						expect(storage.get('foobar')).to.eventually.equal('World'),
 					);
 			});
 		});
@@ -94,20 +94,20 @@ describe('Storage:', () => {
 	describe('.get()', () => {
 		describe('given a key does not exist', () => {
 			it('should eventually be undefined', () =>
-				m.chai.expect(storage.get('foobar')).to.eventually.equal(undefined));
+				expect(storage.get('foobar')).to.eventually.equal(undefined));
 		});
 
 		describe('given a key already exist', () => {
 			beforeEach(() => storage.set('foobar', 'Hello'));
 
 			it('should eventually be the value', () =>
-				m.chai.expect(storage.get('foobar')).to.eventually.equal('Hello'));
+				expect(storage.get('foobar')).to.eventually.equal('Hello'));
 		});
 
 		describe('given getItem throws an error', () => {
 			let getItemStub: any;
 			beforeEach(() => {
-				getItemStub = m.sinon.stub(localStorage, 'getItem');
+				getItemStub = sinon.stub(localStorage, 'getItem');
 				getItemStub.throws(new Error('ENOENT'));
 			});
 
@@ -116,7 +116,7 @@ describe('Storage:', () => {
 			});
 
 			it('should eventually be undefined', () =>
-				m.chai.expect(storage.get('foobar')).to.eventually.equal(undefined));
+				expect(storage.get('foobar')).to.eventually.equal(undefined));
 		});
 
 		describe('given an externally saved value', () => {
@@ -137,13 +137,13 @@ describe('Storage:', () => {
 				});
 
 				it('should be able to read back', () =>
-					m.chai.expect(storage.get('foo')).to.eventually.equal('hello world'));
+					expect(storage.get('foo')).to.eventually.equal('hello world'));
 			});
 
 			describe('with no read access', () => {
 				let fsReadFileStub: any;
 				beforeEach(() => {
-					fsReadFileStub = m.sinon.stub(fs.promises, 'readFile');
+					fsReadFileStub = sinon.stub(fs.promises, 'readFile');
 					fsReadFileStub.rejects({ code: 'EACCES' });
 				});
 
@@ -152,9 +152,9 @@ describe('Storage:', () => {
 				});
 
 				it('should raise an error', () => {
-					return m.chai
-						.expect(storage.get('bar'))
-						.to.eventually.be.rejectedWith(BalenaSettingsPermissionError);
+					return expect(storage.get('bar')).to.eventually.be.rejectedWith(
+						BalenaSettingsPermissionError,
+					);
 				});
 			});
 		});
@@ -163,26 +163,24 @@ describe('Storage:', () => {
 	describe('.has()', () => {
 		describe('given a key does not exist', () => {
 			it('should eventually be false', () =>
-				m.chai.expect(storage.has('foobar')).to.eventually.equal(false));
+				expect(storage.has('foobar')).to.eventually.equal(false));
 		});
 
 		describe('given a key already exist', () => {
 			beforeEach(() => storage.set('foobar', 'Hello'));
 
 			it('should eventually be true', () =>
-				m.chai.expect(storage.has('foobar')).to.eventually.equal(true));
+				expect(storage.has('foobar')).to.eventually.equal(true));
 		});
 	});
 
 	describe('.remove()', () => {
 		describe('given a key does not exist', () => {
 			it('should do nothing', () => {
-				m.chai.expect(storage.has('foobar')).to.eventually.equal(false);
+				expect(storage.has('foobar')).to.eventually.equal(false);
 				return storage
 					.remove('foobar')
-					.then(() =>
-						m.chai.expect(storage.has('foobar')).to.eventually.equal(false),
-					);
+					.then(() => expect(storage.has('foobar')).to.eventually.equal(false));
 			});
 		});
 
@@ -190,13 +188,10 @@ describe('Storage:', () => {
 			beforeEach(() => storage.set('foobar', 'Hello'));
 
 			it('should remove the key', () => {
-				return m.chai
-					.expect(storage.has('foobar'))
+				return expect(storage.has('foobar'))
 					.to.eventually.equal(true)
 					.then(() => storage.remove('foobar'))
-					.then(() =>
-						m.chai.expect(storage.has('foobar')).to.eventually.equal(false),
-					);
+					.then(() => expect(storage.has('foobar')).to.eventually.equal(false));
 			});
 		});
 	});
